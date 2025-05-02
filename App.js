9cb7baa61ae63138711c207d5cd4ca1088a9bb5b
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2025-05-02 00:41:07 -0300

    primeiro commit do projeto

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import api from "./src/services/api";
+import Filmes from './src/components/Filmes';
 
 export default function App() {
+
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes');
+      // console.log(response.data)
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <FlatList
+        data={filmes}
+        keyExtractor={item => String(item.id)} //keyExtractor aceita somente key id como string
+        renderItem={({ item }) => <Filmes data={item} />}
+      />
+
     </View>
   );
 }
@@ -13,8 +34,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingBottom: 50,
   },
 });
